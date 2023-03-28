36711891e1d4d603a1cf48c28457c8171d1e0734
Emil Karlen <emil.karlen@fripost.org>
2023-03-28 12:04:09 +0200

    REFACT show one object

---
+++ src/Wilde/ApplicationConstruction/Presentation/ObjectModel/ObjectComponent.hs
@@ -0,0 +1,80 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Wilde.ApplicationConstruction.Presentation.ObjectModel.ObjectComponent
+(
+  showOneComponent,
+  getShowOneComponent,
+)
+where
+
+
+-------------------------------------------------------------------------------
+-- - import -
+-------------------------------------------------------------------------------
+
+
+import qualified Wilde.Media.Presentation as Presentation
+
+import           Wilde.WildeUi.StdValueTypes as SVT
+import           Wilde.WildeUi.WildeTables
+import           Wilde.WildeUi.UiPrimitives
+import qualified Wilde.WildeUi.WildeStyle as WS
+import           Wilde.WildeUi.WildeTable
+
+import           Wilde.ObjectModel.ObjectModelUtils as OmUtils
+import           Wilde.ObjectModel.Presentation (ATTRIBUTE_PRESENTATION(..))
+
+import qualified Wilde.Render.AbstractTableToHtml
+
+
+-------------------------------------------------------------------------------
+-- - implementation -
+-------------------------------------------------------------------------------
+
+
+getShowOneComponent :: ATTRIBUTE_PRESENTATION atConf
+                    => (Object otConf atConf dbTable otNative idAtExisting idAtCreate
+                    -> [Any (Attribute atConf dbTable)])
+                    -> Object  otConf atConf dbTable otNative idAtExisting idAtCreate
+                    -> Presentation.Monad AnyCOMPONENT
+getShowOneComponent getAttrs o =
+  do
+    let attrs = getAttrs o
+    headerAndValueList <- sequence $ mapAttributeAnyValue headerAndValueOf attrs
+    pure $ showOneComponent headerAndValueList
+  where
+    headerAndValueOf :: ATTRIBUTE_PRESENTATION atConf
+                     => Attribute atConf dbTable typeForExisting typeForCreate
+                     -> Presentation.Monad (Title,AnySVALUE)
+    headerAndValueOf (Attribute at val getPresVal) =
+     do
+       presVal <- getPresVal
+       pure (theTitle,presVal)
+      where
+        theTitle = wildeStyled . atTitle $ at
+        presO = atPresentationO at
+
+showOneComponent :: [(Title,AnySVALUE)] -> AnyCOMPONENT
+showOneComponent  = AnyCOMPONENT . ShowOneComponent
+
+newtype ShowOneComponent = ShowOneComponent [(Title,AnySVALUE)]
+
+instance COMPONENT ShowOneComponent where
+  componentHtml (ShowOneComponent headerAndValueList) =
+    Wilde.Render.AbstractTableToHtml.renderTable styledTable
+    where
+      styledTable   :: WildeTable
+      styledTable   = addStyleToSTYLING WS.presentationTableSingle unstyledTable
+      unstyledTable :: WildeTable
+      unstyledTable  = headerValueTable
+        renderTitle
+        renderValue
+        WS.weAttribute
+        WS.tableColumnStylesShowOne
+        headerAndValueList
+
+      renderTitle :: (Title,AnySVALUE) -> AnySVALUE
+      renderTitle  = AnySVALUE . SVT.UnquotedStringValue . fst
+
+      renderValue :: (Title,AnySVALUE) -> AnySVALUE
+      renderValue  = snd
--- src/Wilde/ObjectModel/Presentation.hs
+++ src/Wilde/ObjectModel/Presentation.hs
@@ -1,24 +1,14 @@
--------------------------------------------------------------------------------
 -- | Presentation of Object's and 'Attribute's.
---
--- TODO: decompose into smaller pieces.
--------------------------------------------------------------------------------
 
 {-# LANGUAGE ScopedTypeVariables #-}
 
 module Wilde.ObjectModel.Presentation
-       (
-         module F,
-
-         ATTRIBUTE_PRESENTATION(..),
-
-         showOneComponent,
-
-         -- * Get attributes from objects
-
-         getAttributesInGivenOrder,
-       )
-       where
+(
+  module F,
+  ATTRIBUTE_PRESENTATION(..),
+  getAttributesInGivenOrder,
+)
+where
 
 
 -------------------------------------------------------------------------------
@@ -26,15 +16,8 @@ module Wilde.ObjectModel.Presentation
 -------------------------------------------------------------------------------
 
 
-import           Wilde.WildeUi.WildeComponent
-import           Wilde.WildeUi.StdValueTypes as SVT
-import           Wilde.WildeUi.WildeTables
-
 import           Wilde.WildeUi.UiPrimitives
-import qualified Wilde.WildeUi.WildeStyle as WS
-import           Wilde.WildeUi.WildeTable
 
-import qualified Wilde.Media.Presentation as Presentation
 import qualified Wilde.ObjectModel.AttributeTypeListSetup.SansAnnotation as AttributeTypeListSetup
 import           Wilde.ObjectModel.ObjectModelUtils as OmUtils
 import           Wilde.ObjectModel.Presentation.FooterRowsConstructor as F
@@ -48,67 +31,6 @@ import           Wilde.ObjectModel.Presentation.FooterRowsConstructor as F
 class ATTRIBUTE_PRESENTATION atConf where
   atTitle :: AttributeType atConf dbTable typeForExisting typeForCreate
           -> WildeTitle
-
-
--------------------------------------------------------------------------------
--- - Show One -
--------------------------------------------------------------------------------
-
-
-showOneComponent :: ATTRIBUTE_PRESENTATION atConf
-                 => (Object otConf atConf dbTable otNative idAtExisting idAtCreate
-                 -> [Any (Attribute atConf dbTable)])
-                 -> Object  otConf atConf dbTable otNative idAtExisting idAtCreate
-                 -> Presentation.Monad AnyCOMPONENT
-showOneComponent getAttrs o =
-  do
-    unstyledTable <- showOneTable (getAttrs o)
-    let styledTable = addStyleToSTYLING WS.presentationTableSingle unstyledTable
-    pure $ AnyCOMPONENT $ TableListComponent styledTable
-
-showOneTable :: ATTRIBUTE_PRESENTATION atConf
-             => [Any (Attribute atConf dbTable)]
-             -> Presentation.Monad WildeTable
-showOneTable attrs =
-  do
-    headerValueRowList <- getHeaderValueRowList
-    pure $
-      headerValueTable
-      renderTitle
-      renderValue
-      WS.weAttribute
-      WS.tableColumnStylesShowOne
-      headerValueRowList
-  where
-    getHeaderValueRowList :: Presentation.Monad [(Title,AnySVALUE)]
-    getHeaderValueRowList = sequence $ mapAttributeAnyValue headerValueRow attrs
-
-    headerValueRow :: ATTRIBUTE_PRESENTATION atConf
-                   => Attribute atConf dbTable typeForExisting typeForCreate
-                   -> Presentation.Monad (Title,AnySVALUE)
-    headerValueRow (Attribute at val getPresVal) =
-     do
-       presVal <- getPresVal
-       pure (theTitle,presVal)
-      where
-        theTitle = wildeStyled . atTitle $ at
-        presO = atPresentationO at
-
-    renderTitle :: (Title,AnySVALUE) -> AnySVALUE
-    renderTitle  = AnySVALUE . SVT.UnquotedStringValue . fst
-
-    renderValue :: (Title,AnySVALUE) -> AnySVALUE
-    renderValue  = snd
-
-
--------------------------------------------------------------------------------
--- - utils -
--------------------------------------------------------------------------------
-
-
-getAtTitle :: ATTRIBUTE_PRESENTATION atConf
-           => Any (AttributeType atConf dbTable) -> WildeTitle
-getAtTitle (Any at) = atTitle at
 
 
 -------------------------------------------------------------------------------
